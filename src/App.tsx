65c182ca9cd09b7374581bccdeaa41dba03a72f4
GarvitSolanki <garvitsolanki5@gmail.com>
2022-04-21 23:50:01 +0530

    Added Pagination

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,11 @@
 import React from 'react'
 import './App.css'
-import DataFetching from './components/DataFetching'
+// import DataFetching from './components/DataFetching'
 import Pagin from './components/Pagin'
 function App() {
   return (
     <div className="App">
-      <DataFetching />
+      {/* <DataFetching /> */}
       <Pagin />
 
       {/* <style>{'body { background-color: black; }'}</style> */}
--- src/components/Pagin.tsx
+++ src/components/Pagin.tsx
@@ -1,6 +1,10 @@
 import { Pagination } from '@mui/material'
-import axios from 'axios'
-import React, { useEffect, useState } from 'react'
+import { Grid } from '@material-ui/core'
+import { makeStyles } from '@material-ui/core/styles'
+import Card from '@mui/material/Card'
+import CardContent from '@mui/material/CardContent'
+// import axios from 'axios'
+import React, {  useState } from 'react'
 
 type User = {
   id: number
@@ -9,20 +13,48 @@ type User = {
   email: string
   avatar: string
 }
+
+const useStyles = makeStyles(theme => ({
+  gridContainer: {
+    // paddingLeft: '40px',
+    // paddingRight: '40px',
+    padding: theme.spacing(2),
+    width: '500px',
+    display: 'flex',
+    textAlign: 'center',
+    justify: 'center'
+  }
+}))
+
 function Pagin() {
   const [users, setUsers] = useState<User[]>([])
-  // const classes = useStyles()
-  useEffect(() => {
-    axios
-      .get('https://reqres.in/api/users?page=2')
-      .then(res => {
-        // console.log(res)
-        setUsers(res.data.data)
-      })
-      .catch(error => {
-        console.log(error)
-      })
-  }, [setUsers])
+  const [pageCount, setPageCount] = useState(1) 
+  const [isLoaded, setisLoaded] = useState(false)
+  const [currentPage, setcurrentPage] = useState(1)
+  
+  const classes = useStyles()
+   const URL = `https://reqres.in/api/users?page=${currentPage}`
+
+   const handleFetch = () => {
+     fetch(URL)
+       .then(response => response.json())
+       .then(body => {
+         setUsers([...body.data])
+         setPageCount(body.total_pages)
+         
+         setisLoaded(true)
+         
+       })
+       .catch(error => console.error('Error', error))
+   }
+
+   const handlePageChange =(event: any,value: React.SetStateAction<number>) => {
+     setcurrentPage(value)
+     handleFetch()
+   }
+   if(!isLoaded)
+    handleFetch()
+
 
   return (
     <div
@@ -32,7 +64,49 @@ function Pagin() {
         textAlign: 'center'
       }}
     >
-      <Pagination count={2} />
+      <Grid
+        container
+        className={classes.gridContainer}
+        spacing={4}
+        justify="center"
+        alignItems="center"
+      >
+        {isLoaded ? 
+        
+          users.map(user => (
+          <Grid item xs={6} sm={6} md={6} key={user.id}>
+            {/* <Box sx={{ minWidth: 275 }}> */}
+            <Card>
+              {/* <CardActionArea> */}
+              {/* <div key={user.id}> */}
+              <img alt={user.first_name} src={user.avatar}></img>
+
+              <div>
+                <CardContent>
+                  {/* <Typography variant="body2" color="text.secondary"> */}
+                  <b>
+                    {user.first_name} {user.last_name}
+                    <br />
+                    {user.email}
+                  </b>
+                  {/* </Typography> */}
+                </CardContent>
+              </div>
+              {/* </div> */}
+            </Card>
+            {/* </Box> */}
+          </Grid>
+        )) : (
+          <div></div>
+        )}
+
+        
+      </Grid>
+      <Pagination
+        count={pageCount}
+        page={currentPage}
+        onChange={handlePageChange}
+      />
     </div>
   )
 }
