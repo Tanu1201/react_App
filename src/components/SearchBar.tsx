a141f226eceed8b91ec0a11b2f7531cd862a1308
Tanu1201 <56487689+Tanu1201@users.noreply.github.com>
2022-04-23 01:24:03 +0530

    Add files via upload

---
+++ src/components/SearchBar.tsx
@@ -0,0 +1,12 @@
+// import axios from 'axios'
+import React from 'react'
+
+function SearchBar() {
+  return (
+    <div className="search">
+      <input type="text" placeholder="Search here" className="searchInput" />
+    </div>
+  )
+}
+
+export default SearchBar
