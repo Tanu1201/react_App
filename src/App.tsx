48d94912fe02641f66260676b2054583bfbe9c69
Tanu1201 <56487689+Tanu1201@users.noreply.github.com>
2022-04-23 01:25:10 +0530

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,21 @@
 import React from 'react'
 import './App.css'
-// import DataFetching from './components/DataFetching'
+import Navbar from './components/Navbar'
 import Pagin from './components/Pagin'
+import SearchBar from './components/SearchBar'
+// import {ThemeProvider, createTheme} from '@material-ui/core/styles'
 function App() {
+  const isBackgroundRed = true
   return (
-    <div className="App">
+    <div className="background-red">
+      <Navbar />
+      <br />
+      <SearchBar />
+      <br />
+
       {/* <DataFetching /> */}
       <Pagin />
-
+      <Navbar />
       {/* <style>{'body { background-color: black; }'}</style> */}
     </div>
   )
